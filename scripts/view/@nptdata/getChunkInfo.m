915fcdb845e5d30021c79a6dae3779e91f3b8763
shihchengyen <30923143+shihchengyen@users.noreply.github.com>
2018-01-22 18:40:26 +0800

    Added bug fix to return default values for output arguments
    Added bug fix to return default values for output arguments when the filelist is empty.

--- scripts/view/@nptdata/getChunkInfo.m
+++ scripts/view/@nptdata/getChunkInfo.m
@@ -1,25 +1,15 @@
 function  [numChunks,chunkSize] = getChunkInfo(sessionname,chunkSize)
 
-
 pdir = pwd;
 p = getDataDirs('session');
 cd(p)
 filelist = nptDir([sessionname '.bin']);
-if isempty(filelist)
-[num_channels,sampling_rate,scan_order]=nptReadStreamerFileHeader(filelist(1).name);
-s.chunkSize = ceil(chunkSize); %round up to the nearest second
-s.numChunks = ceil((filelist(1).bytes-73)/2/num_channels/(chunkSize*sampling_rate)); 
-cd(pdir)
-
-
-
-
-
-
-
-
-
-
-
-
-
+if ~isempty(filelist)
+	[num_channels,sampling_rate,scan_order]=nptReadStreamerFileHeader(filelist(1).name);
+	chunkSize = ceil(chunkSize); %round up to the nearest second
+	numChunks = ceil((filelist(1).bytes-73)/2/num_channels/(chunkSize*sampling_rate)); 
+	cd(pdir)
+else
+	chunkSize = 0;
+	numChunks = 0;
+end
